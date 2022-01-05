ace5f42b9eba7ee6151c550ce640e1ff5d927ea5
krowrasl <66586560+krowrasl@users.noreply.github.com>
2022-01-05 13:57:12 +0300

    Create test0011.cs

---
+++ test0011.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+  public static Dictionary<char, int> Count(string str)
+  {
+    var output = new Dictionary<char, int>();
+    foreach(char letter in str){
+      if(!Char.IsLetter(letter))continue;
+      if(!output.ContainsKey(letter)) 
+        output.Add(letter,1);
+      else
+        output[letter] += 1;
+    }
+    return output;
+  }
+}
+
+/* best practice
+using System.Linq;
+using System.Collections.Generic;
+
+public class Kata
+{
+  public static Dictionary<char, int> Count(string str) => str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+}
+*/
